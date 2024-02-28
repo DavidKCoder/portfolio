fe083eca79d0b807ea111ab1aaedfbc66780d754
DavidKCoder <karapetyand91@gmail.com>
2024-02-28 20:46:14 +0400

    Fix types of level.

--- src/constants.js
+++ src/constants.js
@@ -17,56 +17,56 @@ import wrazzle from "./assets/img/wrazzle.png";
 export const skills = [
   {
     name: 'HTML',
-    level: '95',
+    level: 95,
     info: 'HTML is the standard markup language for creating web pages and web applications. It provides the structure for web content, defining the layout and organization of elements on a page. As a front-end developer, I have a strong understanding of HTML semantics, elements, and attributes, allowing me to create accessible and well-structured web pages.',
     img: html,
     color: 'var(--html)'
   },
   {
     name: 'CSS',
-    level: '90',
+    level: 90,
     info: 'CSS is the styling language used to define the presentation of HTML elements on a web page. With CSS, I can control the appearance, layout, and design of a website, including colors, fonts, spacing, and responsive layouts. I\'m proficient in CSS preprocessors like Sass and familiar with modern CSS frameworks like Bootstrap and Tailwind CSS.',
     img: css,
     color: 'var(--css)'
   },
   {
     name: 'JS',
-    level: '90',
-    info: '  JavaScript is a versatile programming language commonly used for web development. As a developer, I have extensive experience with JavaScript, including building interactive web applications and dynamic user interfaces. I enjoy exploring new features and frameworks within the JavaScript ecosystem to enhance my skills further.\n',
+    level: 90,
+    info: 'JavaScript is a versatile programming language commonly used for web development. As a developer, I have extensive experience with JavaScript, including building interactive web applications and dynamic user interfaces. I enjoy exploring new features and frameworks within the JavaScript ecosystem to enhance my skills further.\n',
     img: javaScript,
     color: 'var(--js)'
   },
   {
     name: 'React.Js',
-    level: '92',
+    level: 92,
     info: 'React is a popular JavaScript library for building user interfaces, known for its component-based architecture and declarative approach to building UIs. As a React developer, I leverage React\'s reusable components, state management, and lifecycle methods to create interactive and dynamic web applications. I\'m experienced in React Hooks, Context API, and integrating third-party libraries and APIs with React applications.',
     img: react,
     color: 'var(--react)'
   },
   {
     name: 'Redux',
-    level: '90',
+    level: 90,
     info: 'Redux is a predictable state container for JavaScript applications, commonly used with React for managing application state. With Redux, I can centralize and manage the state of my application in a single store, making it easier to debug, test, and maintain complex state logic. I\'m experienced in defining actions, reducers, and selectors in Redux, as well as integrating Redux with React applications for efficient state management.',
     img: redux,
     color: 'var(--redux)'
   },
   {
     name: 'Next.Js',
-    level: '90',
+    level: 90,
     info: 'Next.js is a popular React framework for building server-side rendered (SSR) and statically generated (SSG) web applications. With Next.js, I can create SEO-friendly, performance-optimized web apps with features like pre-rendering, routing, and API routes out of the box. I\'m proficient in using Next.js for building scalable and production-ready React applications, leveraging its built-in features like automatic code splitting, image optimization, and data fetching.',
     img: nextJs,
     color: 'var(--next)'
   },
   {
     name: 'Node.Js',
-    level: '80',
+    level: 80,
     info: 'Node.js is a server-side JavaScript runtime environment that enables the execution of JavaScript code outside the browser. With Node.js, I can build scalable and high-performance server-side applications, APIs, and microservices. I\'m proficient in using Node.js with frameworks like Express.js for routing, middleware, and handling HTTP requests, as well as working with databases like MongoDB and MySQL.',
     img: nodeJS,
     color: 'var(--node)'
   },
   {
     name: 'React Native',
-    level: '70',
+    level: 70,
     info: 'React Native is a framework for building native mobile applications using JavaScript and React. It allows developers to create cross-platform mobile apps with a single codebase, targeting both iOS and Android platforms. As a React Native developer, I leverage the power of React\'s component-based architecture to build intuitive and responsive mobile user interfaces.',
     img: react,
     color: 'var(--react)'
--- src/pages/index.js
+++ src/pages/index.js
@@ -22,7 +22,7 @@ export default function Home() {
         <meta property="og:title" content="David K."/>
         <meta property="og:description" content="David K. portfolio"/>
         <meta property="og:image" content="../assets/img/meta_img.png"/>
-        <link rel="icon" href="/public/favicon.ico"/>
+        <link rel="icon" href="/favicon.ico"/>
         <meta name="viewport" content="width=device-width, initial-scale=1"/>
         <link rel="icon" href="/favicon.ico"/>
       </Head>
